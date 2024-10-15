db50488fd9cdabafb8b0b42b55c1b36eb1c2b576
ElectroluxV2 <electroluxv2@gmail.com>
2024-10-15 03:44:34 +0200

    Fix flaky tests

--- SharpValueInjector.Tests/IntegrationTests.cs
+++ SharpValueInjector.Tests/IntegrationTests.cs
@@ -5,29 +5,30 @@ using SharpValueInjector.App;
 
 namespace SharpValueInjector.Tests;
 
-[NotInParallel] // Dont know how to handle temp directories in parallel
 [SuppressMessage("Performance", "CA1822:Mark members as static")]
 public class IntegrationTests
 {
-    private static string _samplesDirectory = string.Empty;
-
     [Before(Test)]
     public Task Setup(TestContext context)
     {
         // Before each test we gonna copy whole Samples directory to temp directory
-        _samplesDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-        FileSystem.CopyDirectory("Samples", _samplesDirectory, true);
+        var tempSamples = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        FileSystem.CopyDirectory("Samples", tempSamples, true);
+        
+        context.ObjectBag["Samples"] = tempSamples; 
 
         return Task.CompletedTask;
     }
+    
+    private static string Samples => TestContext.Current!.ObjectBag["Samples"] as string ?? throw new InvalidOperationException();
 
     [Test]
     public async Task Injection_ShouldWork_WhenInputValueHasSimpleKey()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Simple", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Simple", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
-            [Path.Combine(_samplesDirectory, "Simple", "input.json")],
+            [Path.Combine(Samples, "Simple", "input.json")],
             true,
             false,
             "#{",
@@ -36,7 +37,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Simple", "after.yml");
+        var afterFile = Path.Combine(Samples, "Simple", "after.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -45,10 +46,10 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WhenInputValueHasComplexKey()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Complex", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Complex", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
-            [Path.Combine(_samplesDirectory, "Complex", "input.json")],
+            [Path.Combine(Samples, "Complex", "input.json")],
             true,
             false,
             "#{",
@@ -57,7 +58,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Complex", "after.yml");
+        var afterFile = Path.Combine(Samples, "Complex", "after.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -66,10 +67,10 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WhenInputValueHasReference()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Reference", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Reference", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
-            [Path.Combine(_samplesDirectory, "Reference", "input.json")],
+            [Path.Combine(Samples, "Reference", "input.json")],
             true,
             false,
             "#{",
@@ -78,7 +79,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Reference", "after.yml");
+        var afterFile = Path.Combine(Samples, "Reference", "after.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -87,10 +88,10 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WhenInputValueHasRecursiveReference()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Recursive", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Recursive", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
-            [Path.Combine(_samplesDirectory, "Recursive", "input.json")],
+            [Path.Combine(Samples, "Recursive", "input.json")],
             true,
             false,
             "#{",
@@ -99,7 +100,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Recursive", "after.yml");
+        var afterFile = Path.Combine(Samples, "Recursive", "after.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -108,10 +109,10 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WhenInputValueIsNumeric()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Numeric", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Numeric", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
-            [Path.Combine(_samplesDirectory, "Numeric", "input.json")],
+            [Path.Combine(Samples, "Numeric", "input.json")],
             true,
             false,
             "#{",
@@ -120,7 +121,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Numeric", "after.yml");
+        var afterFile = Path.Combine(Samples, "Numeric", "after.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -129,10 +130,10 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WhenInputValueIsBoolean()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Boolean", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Boolean", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
-            [Path.Combine(_samplesDirectory, "Boolean", "input.json")],
+            [Path.Combine(Samples, "Boolean", "input.json")],
             true,
             false,
             "#{",
@@ -141,7 +142,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Boolean", "after.yml");
+        var afterFile = Path.Combine(Samples, "Boolean", "after.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -150,10 +151,10 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WhenInputHasConflict()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Conflict", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Conflict", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
-            [Path.Combine(_samplesDirectory, "Conflict", "input.json")],
+            [Path.Combine(Samples, "Conflict", "input.json")],
             true,
             false,
             "#{",
@@ -162,7 +163,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Conflict", "after.yml");
+        var afterFile = Path.Combine(Samples, "Conflict", "after.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -172,13 +173,13 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WithHierarchicalInputs_Case1()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Hierarchy", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Hierarchy", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
             [
-                Path.Combine(_samplesDirectory, "Hierarchy", "input-a.json"),
-                Path.Combine(_samplesDirectory, "Hierarchy", "input-b.json"),
-                Path.Combine(_samplesDirectory, "Hierarchy", "input-c.json"),
+                Path.Combine(Samples, "Hierarchy", "input-a.json"),
+                Path.Combine(Samples, "Hierarchy", "input-b.json"),
+                Path.Combine(Samples, "Hierarchy", "input-c.json"),
             ],
             true,
             false,
@@ -188,7 +189,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Hierarchy", "after-c.yml");
+        var afterFile = Path.Combine(Samples, "Hierarchy", "after-c.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
@@ -197,13 +198,13 @@ public class IntegrationTests
     [Test]
     public async Task Injection_ShouldWork_WithHierarchicalInputs_Case2()
     {
-        var beforeFile = Path.Combine(_samplesDirectory, "Hierarchy", "before.yml");
+        var beforeFile = Path.Combine(Samples, "Hierarchy", "before.yml");
         var code = await InjectorApp.BootstrapAsync(
             [beforeFile],
             [
-                Path.Combine(_samplesDirectory, "Hierarchy", "input-c.json"),
-                Path.Combine(_samplesDirectory, "Hierarchy", "input-b.json"),
-                Path.Combine(_samplesDirectory, "Hierarchy", "input-a.json"),
+                Path.Combine(Samples, "Hierarchy", "input-c.json"),
+                Path.Combine(Samples, "Hierarchy", "input-b.json"),
+                Path.Combine(Samples, "Hierarchy", "input-a.json"),
             ],
             true,
             false,
@@ -213,7 +214,7 @@ public class IntegrationTests
             LogLevel.Information
         );
 
-        var afterFile = Path.Combine(_samplesDirectory, "Hierarchy", "after-a.yml");
+        var afterFile = Path.Combine(Samples, "Hierarchy", "after-a.yml");
 
         await Assert.That(code).IsEqualTo(0);
         await Assert.That(beforeFile).IsTheSame(afterFile);
