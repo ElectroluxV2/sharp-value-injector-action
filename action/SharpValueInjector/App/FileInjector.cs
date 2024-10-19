e98fee7b0349990a4bbae3b3dc2b3c5cb5a1c3b6
ElectroluxV2 <electroluxv2@gmail.com>
2024-10-19 16:05:48 +0200

    Either Hot Shit or Total Bollocks

--- action/SharpValueInjector/App/FileInjector.cs
+++ action/SharpValueInjector/App/FileInjector.cs
@@ -11,21 +11,22 @@ public class FileInjector(ILogger<FileInjector> logger)
         {
             logger.LogInformation("Injecting values into file {Path}", path);
             using var reader = File.OpenText(path);
-            await using var writer = File.CreateText($"{path}.injected");
-
-            while (!reader.EndOfStream)
+            await using (var writer = File.CreateText($"{path}.injected"))
             {
-                var line = await reader.ReadLineAsync(cancellationToken);
-                if (line is null) break;
-
-                // PERF: It should be possible to replace this within a single pass
-                var sb = new StringBuilder(line);
-                foreach (var (key, value) in injections)
+                while (!reader.EndOfStream)
                 {
-                    sb.Replace($"{openingToken}{key}{closingToken}", value.AsSpan());
-                }
+                    var line = await reader.ReadLineAsync(cancellationToken);
+                    if (line is null) break;
 
-                await writer.WriteLineAsync(sb.ToString());
+                    // PERF: It should be possible to replace this within a single pass
+                    var sb = new StringBuilder(line);
+                    foreach (var (key, value) in injections)
+                    {
+                        sb.Replace($"{openingToken}{key}{closingToken}", value.AsSpan());
+                    }
+
+                    await writer.WriteLineAsync(sb.ToString());
+                }
             }
 
             File.Replace($"{path}.injected", path, null);
