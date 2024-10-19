636c8d97e7f4c124f432b5a04566bf6534b9a993
ElectroluxV2 <electroluxv2@gmail.com>
2024-10-19 15:02:34 +0200

    I'm always trying to show versatility.

--- action/SharpValueInjector/Program.cs
+++ action/SharpValueInjector/Program.cs
@@ -4,17 +4,56 @@ using SharpValueInjector.App;
 using Spectre.Console;
 using static SharpValueInjector.Shared.ConsoleLifetimeUtils;
 
-var outputFilesArgument = new Argument<string[]>("output", "Files to inject values into or directories to scan (may contain file name patterns such as '/sample/path/*.yaml'.).");
-var inputFilesOption = new Option<string[]>("--input", "Path to JSON file or directories to scan (may contain file name patterns such as '/sample/path/*.json'.) that contain values to inject into target files. To specify multiple files, use multiple --input options. Order matters when resolving conflicts.")
+var outputFilesArgument = new Argument<string[]>(
+    "output",
+    () => ArrayFromEnv("SVI_OUTPUT"),
+    "Files to inject values into or directories to scan (may contain file name patterns such as '/sample/path/*.yaml'.)."
+);
+
+var inputFilesOption = new Option<string[]>(
+    "--input",
+    () => ArrayFromEnv("SVI_INPUT"),
+    "Path to JSON file or directories to scan (may contain file name patterns such as '/sample/path/*.json'.) that contain values to inject into target files. To specify multiple files, use multiple --input options. Order matters when resolving conflicts."
+)
 {
     IsRequired = true,
 };
-var recurseSubdirectoriesOption = new Option<bool>("--recurse-subdirectories", () => true, "Weather to scan subdirectories of the given directories.");
-var ignoreCaseOption = new Option<bool>("--ignore-case", () => true, "Weather to ignore case when matching file name patterns.");
-var openingTokenOption = new Option<string>("--opening-token", () => "#{", "The opening token for variable interpolation.");
-var closingTokenOption = new Option<string>("--closing-token", () => "}", "The closing token for variable interpolation.");
-var awsSmTokenOption = new Option<string>("--aws-sm-token", "The AWS Secrets Manager token to use for fetching secrets. When not provided, AWS SM secrets detection & fetching is disabled.");
-var logLevelOption = new Option<LogLevel>("--log-level",  () => LogLevel.Information, "The minimum level of logs to output.");
+
+var recurseSubdirectoriesOption = new Option<bool>(
+    "--recurse-subdirectories",
+    () => BoolFromEnv("SVI_RECURSE_SUBDIRECTORIES") ?? true,
+    "Weather to scan subdirectories of the given directories."
+);
+
+var ignoreCaseOption = new Option<bool>(
+    "--ignore-case",
+    () => BoolFromEnv("SVI_IGNORE_CASE") ?? true,
+    "Weather to ignore case when matching file name patterns."
+);
+
+var openingTokenOption = new Option<string>(
+    "--opening-token",
+    () => Environment.GetEnvironmentVariable("SVI_OPENING") ?? "#{",
+    "The opening token for variable interpolation."
+);
+
+var closingTokenOption = new Option<string>(
+    "--closing-token",
+    () => Environment.GetEnvironmentVariable("SVI_CLOSING") ?? "}",
+    "The closing token for variable interpolation."
+);
+
+var awsSmTokenOption = new Option<string>(
+    "--aws-sm-token",
+    "The AWS Secrets Manager token to use for fetching secrets. When not provided, AWS SM secrets detection & fetching is disabled."
+);
+
+var logLevelOption = new Option<LogLevel>(
+    "--log-level",
+    () => Enum.Parse<LogLevel>(Environment.GetEnvironmentVariable("SVI_LOG_LEVEL") ?? "Information"),
+    "The minimum level of logs to output."
+);
+
 var root = new RootCommand("Injects values from given inputs into given files. Supports hierarchical conflict resolution, recursive directory scanning, file name patterns, recursive variable interpolation, secrets fetching, usage statistics.")
 {
     inputFilesOption,
@@ -46,4 +85,12 @@ root.SetHandler(async (outputFiles, inputFiles, recurseSubdirectories, ignoreCas
     }
 }, outputFilesArgument, inputFilesOption, recurseSubdirectoriesOption, ignoreCaseOption, openingTokenOption, closingTokenOption, awsSmTokenOption, logLevelOption);
 
-await root.InvokeAsync(args);
+return await root.InvokeAsync(args);
+
+bool? BoolFromEnv(string variable)
+{
+    var value = Environment.GetEnvironmentVariable(variable);
+    return value is not null ? bool.Parse(value) : null;
+}
+
+string[] ArrayFromEnv(string variable) => Environment.GetEnvironmentVariable(variable)?.Split(";").Select(x => x.Trim()).ToArray() ?? [];
