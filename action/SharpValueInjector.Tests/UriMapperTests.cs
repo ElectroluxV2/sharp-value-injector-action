cd5ab793ed5c59fbfee114702e6be5fdf16b878d
ElectroluxV2 <electroluxv2@gmail.com>
2024-10-28 20:37:24 +0100

    1 more assert

--- action/SharpValueInjector.Tests/UriMapperTests.cs
+++ action/SharpValueInjector.Tests/UriMapperTests.cs
@@ -13,11 +13,12 @@ public class UriMapperTests
         var serviceProvider = InjectorApp.BuildServiceProvider([], [], false, false, string.Empty, string.Empty, default, LogLevel.Debug, cancellationToken);
         var uriConsumer = serviceProvider.GetRequiredService<UriMapper>();
 
-        var request = uriConsumer.ToHttpRequest(new("https://api.github.com/electroluxv2/sharp-value-injector-action/repos/contents/.github/workflows/main.yml?ref=main&ref=v2#headers='Accept: application/vnd.github.raw'&headers='X-GitHub-Api-Version: 2022-11-28'&headers='Authorization: Bearer github_pat_11BitStudios_Broke_My_Heart'"));
+        var request = uriConsumer.ToHttpRequest(new("https://api.github.com/electroluxv2/sharp-value-injector-action/repos/contents/.github/workflows/main.yml?ref=main&ref=v2#headers='Accept: application/vnd.github.raw'&headers='X-GitHub-Api-Version: 2022-11-28'&headers=User-Agent: SharpValueInjector&headers='Authorization: Bearer github_pat_11BitStudios_Broke_My_Heart'"));
         await Assert.That(request.Method).IsEqualTo(HttpMethod.Get);
         await Assert.That(request.RequestUri!.AbsoluteUri).IsEqualTo("https://api.github.com/electroluxv2/sharp-value-injector-action/repos/contents/.github/workflows/main.yml?ref=main&ref=v2");
         await Assert.That(request.Headers.GetValues("Accept")).Contains("application/vnd.github.raw");
         await Assert.That(request.Headers.GetValues("X-GitHub-Api-Version")).Contains("2022-11-28");
         await Assert.That(request.Headers.GetValues("Authorization")).Contains("Bearer github_pat_11BitStudios_Broke_My_Heart");
+        await Assert.That(request.Headers.GetValues("User-Agent")).Contains("SharpValueInjector");
     }
 }
