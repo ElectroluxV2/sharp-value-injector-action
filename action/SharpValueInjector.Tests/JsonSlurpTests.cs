8d17389f19b4088f75c7ba026268dd06c058a6d7
ElectroluxV2 <electroluxv2@gmail.com>
2024-11-28 19:28:57 +0100

    nk

--- action/SharpValueInjector.Tests/JsonSlurpTests.cs
+++ action/SharpValueInjector.Tests/JsonSlurpTests.cs
@@ -43,30 +43,4 @@ public class JsonSlurpTests
         await Assert.That(result["b.c"]).IsEqualTo("2");
         await Assert.That(result["b.d.e"]).IsEqualTo("3");
     }
-
-    [Test]
-    public async Task SM()
-    {
-        var client = new AmazonSecretsManagerClient();
-        var request = new BatchGetSecretValueRequest
-        {
-            SecretIdList = [
-                "QA/Module/AutoInsight",
-                // "arn:aws:secretsmanager:eu-west-1:000209418239:secret:QA/Module/AutoInsight-S75Vg7",
-                // "arn:aws:secretsmanager:us-west-2:000209418239:secret:QA/Component/AutoInsight.Data.Service-CLmJj5",
-                "arn:aws:secretsmanager:eu-west-1:000209418239:secret:QA/Meta/Connect-hCvfvU",
-            ],
-        };
-
-        var response = await client.BatchGetSecretValueAsync(request);
-
-        await Assert.That(response).IsNotNull();
-
-        foreach (var value in response.SecretValues)
-        {
-            await Assert.That(value.SecretString).IsNotNull();
-            await Assert.That(value.SecretString.Length).IsGreaterThan(0);
-            Console.Out.WriteLine($"SecretString: ({value.Name}) " + value.SecretString);
-        }
-    }
 }
